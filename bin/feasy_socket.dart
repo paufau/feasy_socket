ca2a9ed5c5090fb51e60ec6ccd89255d604970d3
paufau <pavelpakseev@MacBook-Pro-Pavel.local>
2023-01-23 23:16:17 +0300

    Initial commit

---
+++ bin/feasy_socket.dart
@@ -0,0 +1,5 @@
+import 'package:feasy_socket/feasy_socket.dart' as feasy_socket;
+
+void main(List<String> arguments) {
+  print('Hello world: ${feasy_socket.calculate()}!');
+}
---
+++ test/feasy_socket_test.dart
@@ -0,0 +1,8 @@
+import 'package:feasy_socket/feasy_socket.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
